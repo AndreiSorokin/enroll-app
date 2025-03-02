80491536baaa7a94920588b530d9318f8a785201
Andrei Sorokin <72672144+AndreiSorokin@users.noreply.github.com>
2025-03-02 15:40:56 +0200

    m

--- server/src/utils/config.ts
+++ server/src/utils/config.ts
@@ -2,5 +2,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const DATABASE_URL = process.env.DATABASE_URL as string;
-export const PORT = process.env.PORT as string;
+export const PORT = process.env.PORT || 3009;
 export const JWT_SECRET = process.env.JWT_SECRET as string;
