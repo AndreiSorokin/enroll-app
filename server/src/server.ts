4132b51e3c52eb7b49842d8821e9990cc6a277cb
Andrei Sorokin <72672144+AndreiSorokin@users.noreply.github.com>
2025-03-03 20:11:52 +0200

    start command modified for monolith

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,18 @@
+import express from "express";
+import path from 'path';
+
 import app from './app';
 import { PORT } from './utils/config';
 import { connectToDatabase } from './utils/db';
+
+const clientBuildPath = path.join(__dirname, '../client/dist');
+
+app.use(express.static(clientBuildPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(clientBuildPath, 'index.html'));
+});
+
 
 const start = async () => {
    await connectToDatabase();
